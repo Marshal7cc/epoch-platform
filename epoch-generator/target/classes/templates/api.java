7986a028ee771dd739d0f47f73e74d4cff37ddad
Marshal <marshal960818@gmail.com>
2022-10-22 23:10:37 +0800

    feat: project init

---
+++ epoch-generator/target/classes/templates/api.java
@@ -0,0 +1,10 @@
+package [(${package})].api;
+
+import org.epoch.mybatis.common.CommonApi;
+import [(${package})].domain.entity.[(${entityName})];
+
+/**
+ * @author [(${authorName})]
+ */
+public interface [(${apiName})] extends CommonApi<[(${entityName})]>{
+}
---
+++ epoch-generator/target/classes/templates/repository.java
@@ -0,0 +1,11 @@
+package [(${package})].service;
+
+import org.epoch.mybatis.repository.BaseRepository;
+import [(${package})].domain.entity.[(${entityName})];
+
+/**
+ * @author [(${authorName})]
+ */
+public interface [(${repositoryName})] extends BaseRepository<[(${entityName})]>{
+
+}
